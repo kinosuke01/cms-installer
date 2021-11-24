62703631344845c1cd8ea9b7ddc9414b22c20661
kinosuke01 <kinosuke01@pepabo.com>
2021-11-24 20:35:54 +0900

    add config

---
+++ basercms/const.go
@@ -0,0 +1,8 @@
+package basercms
+
+const initArchiveURL string = "https://basercms.net/packages/download_exec/basercms-4.5.3.zip"
+const initArchiveDir string = "basercms"
+const initScript string = "init.php"
+
+const bcHttpTimeout int = 5
+const initHttpTimeout int = 15
