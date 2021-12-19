27212c4b0dae750b12abc57867d99708e27e4d93
kinosuke01 <kinosuke01@pepabo.com>
2021-12-20 07:00:48 +0900

    update

--- basercms/php/install.php
+++ basercms/php/install.php
@@ -1,46 +1,23 @@
 <?php
+ini_set('display_errors', "Off");
+
 $phpPath = "php";
-$cmd     = getcwd() . '/app/Console/cake.php';
+$cake    = getcwd() . '/app/Console/cake.php';
 $working = getcwd() . '/app';
 
-// sh ./app/Console/cake bc_manager install -app /Users/kinosuke01/Desktop/basercms/app "http://site.url" "dbtype" "username" "password" "email" --host "hostname" --database "dbname" --login "dbuser" --password "dbpassword" --prefix "prefix_" --port "portnumber" --baseurl "/" --data "nada-icons.default"
+$cmd  = "$phpPath -q $cake -woking $working bc_manager install ";
+// TODO
+$cmd .= "http://site.url dbtype username password email --host hostname --database dbname --login dbuser --password dbpassword --prefix prefix_ --port portnumber --baseurl / --data nada-icons.default ";
+$cmd .= "2>&1";
 
-// exec php -q "$CONSOLE"/cake.php -working "$APP" "$@"
-
-ini_set('display_errors', "On");
-$argv = [
-  (getcwd() . "/app/Console/cake.php"),
-  "-working",
-  (getcwd() . "/app"),
-  "bc_manager",
-  "install",
-  "http://site.url",
-  "dbtype",
-  "username",
-  "password",
-  "email",
-  "--host",
-  "hostname",
-  "--database",
-  "dbname",
-  "--login",
-  "dbuser",
-  "--password",
-  "dbpassword",
-  "--prefix",
-  "prefix_",
-  "--port",
-  "portnumber",
-  "--baseurl",
-  "/",
-  "--data",
-  "nada-icons.default"
-];
-if (!defined('DS')) {
-	define('DS', DIRECTORY_SEPARATOR);
+$output = [];
+$exit = 0;
+if (!exec($cmd, $output, $exit)) {
+  $output[] = 'unknown_error';
 }
-$dispatcher = 'lib' . DS . 'Cake' . DS . 'Console' . DS . 'ShellDispatcher.php';
 
-include($dispatcher);
-unset($dispatcher);
-return ShellDispatcher::run($argv);
+$res = [
+  'exit_code' => $exit,
+  'messages' => $output,
+];
+echo json_encode($res);
