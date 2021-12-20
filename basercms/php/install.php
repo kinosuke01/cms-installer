8a2b77a7a1602fb7565d1a73f7290d55dbe73dd2
kinosuke01 <kinosuke01@pepabo.com>
2021-12-20 18:38:44 +0900

    update

--- basercms/php/install.php
+++ basercms/php/install.php
@@ -1,20 +1,37 @@
 <?php
 ini_set('display_errors', "Off");
 
-$phpPath = "php";
-$cake    = getcwd() . '/app/Console/cake.php';
+function build_cmd($params = array()) {
+  $phpPath = "php";
+  $cake    = getcwd() . '/app/Console/cake.php';
 
-$cmd  = "$phpPath -q $cake bc_manager install ";
-// TODO
-$cmd .= "http://site.url dbtype username password email --host hostname --database dbname --login dbuser --password dbpassword --prefix prefix_ --port portnumber --baseurl / --data nada-icons.default ";
-$cmd .= "2>&1";
+  $cmds = ["$phpPath -q $cake bc_manager install"];
 
+  $reqKeys = ['siteurl', 'dbtype', 'username', 'password'];
+  foreach($reqKeys as $key) {
+    $val = isset($params[$key]) ? $params[$key] : '';
+    $cmds[] = escapeshellarg($val);
+  }
+
+  $optKeys = ['host', 'database', 'login', 'password', 'dbpassword', 'prefix', 'port', 'portnumber', 'baseurl', 'data'];
+  foreach($optKeys as $key) {
+    if (isset($params[$key])) {
+      $cmds[] = '--' . $key . ' ' . escapeshellarg($params[$key]);
+    }
+  }
+
+  $cmds[] = '2>&1';
+
+  $cmd = implode(' ', $cmds);
+  return $cmd;
+}
+
+$cmd = build_cmd($_POST);
 $output = [];
 $exit = 0;
 if (!exec($cmd, $output, $exit)) {
   $output[] = 'unknown_error';
 }
-
 $res = [
   'exit_code' => $exit,
   'messages' => $output,
